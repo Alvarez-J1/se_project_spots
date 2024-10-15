b5009357b1edb42c1745ba5faad72646c7c03992
Joel Alvarez <Joel124@outlook.es>
2024-10-15 13:22:11 -0400

    Stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,20 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector(".profile__edit-btn");
+
+const editProfileModal = document.querySelector("#edit-profile-modal");
+
+const closeButtonModal = editProfileModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editProfileModal.classList.add("modal__opened");
+}
+
+function closeModal() {
+  editProfileModal.classList.remove("modal__opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+closeButtonModal.addEventListener("click", closeModal);
