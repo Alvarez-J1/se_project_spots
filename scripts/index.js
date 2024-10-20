e355894e4699f1001d6de4f678d8ebf610ca40f3
Joel Alvarez <Joel124@outlook.es>
2024-10-20 04:52:27 -0400

    Fixed modal_opened element

--- scripts/index.js
+++ scripts/index.js
@@ -64,11 +64,11 @@ function openModal() {
   editModalNameInput.value = profileNameElement.textContent;
   editModalDescriptionInput.value = profileDescriptionElement.textContent;
 
-  editProfileModal.classList.add("modal__opened");
+  editProfileModal.classList.add("modal_opened");
 }
 
 function closeModal() {
-  editProfileModal.classList.remove("modal__opened");
+  editProfileModal.classList.remove("modal_opened");
 }
 
 function handleEditFormSubmit(evt) {
